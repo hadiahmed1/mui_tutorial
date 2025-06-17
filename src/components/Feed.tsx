7c6db0b0586f6ffd467a00be8dccf4843a09427f
Hadi <969hadiahmed@gmail.com>
2025-06-17 16:31:53 +0530

    feat: Feed Card

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,49 @@
-import { Box } from "@mui/material";
-
-const Feed = () =>{
-    return (<Box flex={4} p={2} >Feed</Box>);
+import { Avatar, Box, Card, CardActions, CardContent, CardHeader, CardMedia, Checkbox, IconButton, Typography } from "@mui/material";
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { red } from "@mui/material/colors";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+const Feed = () => {
+    return (
+        <Box flex={4} p={2} >
+            <Card sx={{ maxWidth: 345 }}>
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                            R
+                        </Avatar>
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                    title="Shrimp and Chorizo Paella"
+                    subheader="September 14, 2016"
+                />
+                <CardMedia
+                    component="img"
+                    height="30%"
+                    image="https://mui.com/static/images/cards/paella.jpg"
+                    alt="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        This impressive paella is a perfect party dish and a fun meal to cook
+                        together with your guests. Add 1 cup of frozen peas along with the mussels,
+                        if you like.
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <Checkbox {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+                    <IconButton aria-label="share">
+                        <ShareIcon />
+                    </IconButton>
+                </CardActions>
+            </Card>
+        </Box>
+    );
 }
 
 export default Feed;
