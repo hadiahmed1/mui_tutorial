3c664e141639fb379a5f430b1ed72e5bda334eca
Hadi <969hadiahmed@gmail.com>
2025-06-17 15:53:47 +0530

    feat: SideBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import Feed from "./components/Feed"
 import Navbar from "./components/Navbar"
 import RightBar from "./components/RightBar"
 import SideBar from "./components/SideBar"
+import { useState } from "react"
+
+export type theme = "light" | "dark";
 
 function App() {
+  const [mode, setMode] = useState<theme>("light");
   return (
     <Box>
       <Navbar />
-      <Stack direction="row" spacing="2" justifyContent="space-between" divider={<Divider orientation="vertical"  flexItem />} >
-        <SideBar />
+      <Stack direction="row" spacing="2" justifyContent="space-between" divider={<Divider orientation="vertical" flexItem />} >
+        <SideBar mode={mode} setMode={setMode} />
         <Feed />
         <RightBar />
       </Stack>
